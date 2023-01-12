44d2ee441ef502f6d579dc1dc31bedd91edbd2b1
Łukasz <109048586+zwozlukasz@users.noreply.github.com>
2023-01-12 15:58:34 +0000

    lambda function

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 # functions
 
+
 def func_1():
   print("Call function_1")
   return func_2()
 
+
 def func_2():
   print("Call function_2")
 
+
 # we have to define func_2() before we decide invoke func_1
 func_1()
+
+func_lambda = lambda a: a**2
+print(f"value returned from lambda function: {func_lambda(4)}")
